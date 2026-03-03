730332b6ffbeb25fd0c892f9b297d2dd06b1b99f
VDhanush75 <dhanushdhanush76593@gmail.com>
2026-03-03 22:07:59 +0530

    Restrict CORS origins to specific local addresses

--- backend/src/app.js
+++ backend/src/app.js
@@ -62,7 +62,12 @@ app.use(limiter);
 
 // allow CORS for all origins (for development)
 app.use(cors({
-  origin: "*",
+  // origin: "*",
+   origin: [
+    "http://localhost:5173",
+    "http://localhost:8080",
+    "http://10.192.137.104:8080"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
